6e422b916b27f8bada5580270dacc8d3ab8a85bc
Gabriel Leung <leun0077@algonquinlive.com>
2019-03-09 01:23:06 -0500

    Worked-on-router-and-register-page

--- src/router.js
+++ src/router.js
@@ -1,17 +1,36 @@
-import VueRouter from 'vue-router';
-import loginPage from './components/pages/login-page/login-page.component.vue';
+import VueRouter from 'vue-router'
+import LoginPage from './components/pages/login-page/login-page.component.vue'
+import RegisterPage from './components/pages/register-page/register-page.component.vue'
+import DashboardPage from './components/pages/dashboard-page/dashboard-page.component.vue'
+import ListPage from './components/pages/list-page/list-page.component.vue'
 
 const router = new VueRouter({
   routes: [
     {
       path: '/',
-      redirect: '/login',
+      redirect: '/login'
     },
     {
       path: '/login',
-      component: loginPage
+      component: LoginPage
     },
+    {
+      path: '/register',
+      component: RegisterPage
+    },
+    {
+      path: '/',
+      redirect: '/dashboard'
+    },
+    {
+      path: '/dashboard',
+      component: DashboardPage
+    },
+    {
+      path: '/list',
+      component: ListPage
+    }
   ]
-});
+})
 
-export default router;
+export default router
